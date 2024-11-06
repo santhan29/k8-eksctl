4c2bb9f04a3f946a76712eb0bee4a3f8aedc9771
santhan29 <santhanpeddaguravagari@gmail.com>
2024-11-06 12:44:09 +0530

    helm-charts

--- disk-resize.sh
+++ disk-resize.sh
@@ -28,5 +28,5 @@ git clone https://github.com/ahmetb/kubectx /opt/kubectx
 ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-$ chmod 700 get_helm.sh
-$ ./get_helm.sh 
+chmod 700 get_helm.sh
+./get_helm.sh 
